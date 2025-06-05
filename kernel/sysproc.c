3c0396995ed4824b5c63df5d6c2c0adde3df9156
LihieNiv1 <lihieniv12@gmail.com>
2025-06-05 20:53:43 +0300

    pgtbl done

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -12,7 +12,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -43,7 +43,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -54,12 +54,13 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
   argint(0, &n);
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -69,12 +70,25 @@ sys_sleep(void)
   return 0;
 }
 
-
+extern void get_pgaccess(pagetable_t pagetable, uint64 init_va, uint16 num_pages, void *buffer);
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  char buffer[1 << 13];
+  pagetable_t pagetable = myproc()->pagetable;
+  uint64 va;
+  uint64 user_buffer;
+  int num_pages;
+  argaddr(0, &va);
+  argint(1, &num_pages);
+  argaddr(2, &user_buffer);
+  if (num_pages >= (1 << 16))
+  {
+    return -1;
+  }
+  get_pgaccess(pagetable, va, (uint16)num_pages, buffer);
+  copyout(pagetable, user_buffer, buffer, (uint16)(num_pages >> 3));
   return 0;
 }
 #endif
